6979fa6ec22942677bd44beb0100cf41d53bc8a3
Taras Pakizh <pakizht@gmail.com>
2018-08-29 11:03:28 +0300

    tests

---
+++ Pakizh.Taras.RobotChallenge.Test/HelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pakizh.Taras.RobotChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pakizh.Taras.RobotChallenge.Tests
+{
+    [TestClass()]
+    public class HelperTests
+    {
+        [TestMethod()]
+        public void FindDistanceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CanCollectTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Pakizh.Taras.RobotChallenge.Test/PakizhTarasAlgorithmTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pakizh.Taras.RobotChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pakizh.Taras.RobotChallenge.Tests
+{
+    [TestClass()]
+    public class PakizhTarasAlgorithmTests
+    {
+        [TestMethod()]
+        public void PakizhTarasAlgorithmTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PakizhTarasAlgorithmTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DoStepTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNextPositionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideWayBy2Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindNearestFreeStationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindNearestOccupiedStationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindNearestFreeCellAroundStationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsCellFreeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsStationFreeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsCollectingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsCellValidTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
